c76a3cf1c3e8569416381bc05bcb80db2089c664
MiksuJak <43791318+MiksuJak@users.noreply.github.com>
2025-05-21 17:45:25 +0200

    🥐 Use loaded private key

--- index.ts
+++ index.ts
@@ -14,13 +14,13 @@ const sourceChain = optimism
 const targetChain = base
 const targetVaultAddress = '0x3128a0F7f0ea68E7B7c9B00AFa7E41045828e858' as Address // has to be a USDC vault
 const usdcAddress = getTokenAddress('USDC', targetChain.id)
-const targetUsdcAmount = parseUnits('1', 6) // 1 USDC
+const targetUsdcAmount = parseUnits('0.1', 6) // 0.1 USDC
 
 
 const main = async () => {
   // --- SMART ACCOUNT SETUP ---
-  const fundingPrivateKey = process.env.PRIVATE_KEY
-  if (!fundingPrivateKey) {
+  const privateKey = process.env.PRIVATE_KEY as Hex
+  if (!privateKey) {
     throw new Error('PRIVATE_KEY is not set')
   }
 
@@ -38,7 +38,6 @@ const main = async () => {
   }
   
   // You can use an existing PK here
-  const privateKey = generatePrivateKey()
   console.log(`Owner private key: ${privateKey}`)
   const account = privateKeyToAccount(privateKey)
   
@@ -50,7 +49,7 @@ const main = async () => {
     rhinestoneApiKey,
   })
   const smartAccountAddress = rhinestoneAccount.getAddress()
-  console.log(`Smart account address: ${rhinestoneAccount.config.account}`)
+  console.log(`Smart account address: ${smartAccountAddress}`)
 
   // --- GETTING DEPOSIT DATA ---
   const depositActions = await vaultsSdk.getActions('/v1/transactions/vaults/deposit', {
